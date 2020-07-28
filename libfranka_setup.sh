c918ff0ba7a6ca50a6dfa59d8cbb04e6b085b986
abhinavgandhi09 <abhinavgandhi09@gmail.com>
2020-07-28 06:38:15 -0400

    Added libfranka and franka ros install

--- libfranka_setup.sh
+++ libfranka_setup.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+# This script installs libfranka and frankaros
+
+#### ONLY USE THIS SCRIPT IF SETTING UP THE COMMON COMPUTER FOR THE ROBOT ####
+cd $HOME;
+sudo apt install ros-melodic-libfranka &&
+sudo apt install ros-melodic-franka-ros &&
+echo "Installed franka_ros and libfranka";
