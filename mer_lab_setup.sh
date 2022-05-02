92ca9519a692172d544217b334e4fbaf105adaa9
Abhinav <agandhi2@wpi.edu>
2022-05-02 08:59:08 -0400

    changed to noetic

--- mer_lab_setup.sh
+++ mer_lab_setup.sh
@@ -6,7 +6,7 @@ source ~/.bashrc &&
 git clone https://www.github.com/berkcalli/mer_lab.git &&
 echo "Cloned MER LAB REPOSITORY";
 cd mer_lab &&
-rosdep install --from-paths --ignore-src ros_ws -y --rosdistro melodic &&
+rosdep install --from-paths --ignore-src ros_ws -y --rosdistro noetic &&
 echo "INSTALLED DEPENDENCIES";
 cd ros_ws &&
 catkin_make &&
